9ee3acccd3a8c5abb5da855bf5b97589a68cad76
vagrant <vagrant@vagrant.vm>
2018-09-12 05:11:30 +0000

    pycodestyle, readme fixeds

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This is a log report project using _python_ and _PostgreSQL_.
 
 Created 2 views for my querys.
 
-`create view error_count select TO_CHAR(time::DATE, 'dd/mm/yy') as day, count(TO_CHAR(time::DATE, 'dd/mm/yy')) as cnt from log group by day, status having status != '200 OK'`
+`create view error_count as select TO_CHAR(time::DATE, 'dd/mm/yy') as day, count(TO_CHAR(time::DATE, 'dd/mm/yy')) as cnt from log group by day, status having status != '200 OK'`
 
 `create view success_count as select TO_CHAR(time::DATE, 'dd/mm/yy') as day, count(TO_CHAR(time::DATE, 'dd/mm/yy')) as cnt from log group by day, status having status = '200 OK'`
--- reporter.py
+++ reporter.py
@@ -6,6 +6,7 @@ import psycopg2
 
 db = psycopg2.connect("dbname=news")
 c = db.cursor()
+
 sql = "select substring(path from 10) as slug, count(path) as views \
     from log group by path \
     having path not in ('/', '/spam-spam-spam-humbug', '&20%20%20') \
@@ -15,26 +16,26 @@ logs = c.fetchall()
 print("")
 print("The Most 3 Popular Articles")
 for log in logs:
-  slug = log[0]
-  sql = "select title from articles where slug=%s"
-  c.execute(sql, [slug])
-  articles = c.fetchall()
-  title = articles[0][0]
-  print("Title : {} | view : {}".format(title, log[1]))
+    slug = log[0]
+    sql = "select title from articles where slug=%s"
+    c.execute(sql, [slug])
+    articles = c.fetchall()
+    title = articles[0][0]
+    print("Title : {} | view : {}".format(title, log[1]))
 
 print("")
 print("The Most 3 Popular Authors")
 for log in logs:
-  slug = log[0]
-  sql = "select author from articles where slug=%s"
-  c.execute(sql, [slug])
-  article = c.fetchall()
-  author_id = article[0][0]
-  sql = "select name from authors where id=%s"
-  c.execute(sql, [author_id])
-  author = c.fetchall()
-  author_name = author[0][0]
-  print("Author : {} | view : {}".format(author_name, log[1]))
+    slug = log[0]
+    sql = "select author from articles where slug=%s"
+    c.execute(sql, [slug])
+    article = c.fetchall()
+    author_id = article[0][0]
+    sql = "select name from authors where id=%s"
+    c.execute(sql, [author_id])
+    author = c.fetchall()
+    author_name = author[0][0]
+    print("Author : {} | view : {}".format(author_name, log[1]))
 
 # Created 2 views
 sql = "select sc.day, round((100.0 * (ec.cnt::numeric/sc.cnt::numeric)), 1) \
