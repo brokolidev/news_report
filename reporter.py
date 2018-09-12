f000de41bfeec22e9f29631a6617edb5f27b703c
vagrant <vagrant@vagrant.vm>
2018-09-12 07:34:16 +0000

    Bug fixes and changed queries

--- reporter.py
+++ reporter.py
@@ -7,35 +7,29 @@ import psycopg2
 db = psycopg2.connect("dbname=news")
 c = db.cursor()
 
-sql = "select substring(path from 10) as slug, count(path) as views \
-    from log group by path \
-    having path not in ('/', '/spam-spam-spam-humbug', '&20%20%20') \
-    order by views desc limit 3"
+sql = " select a.title, count(l.path) as cnt from log as l \
+        join articles as a \
+        on l.path=CONCAT('/article/',a.slug) \
+        group by l.path, a.title order by count(l.path) desc limit 3 "
 c.execute(sql)
-logs = c.fetchall()
+rows = c.fetchall()
 print("")
 print("The Most 3 Popular Articles")
-for log in logs:
-    slug = log[0]
-    sql = "select title from articles where slug=%s"
-    c.execute(sql, [slug])
-    articles = c.fetchall()
-    title = articles[0][0]
-    print("Title : {} | view : {}".format(title, log[1]))
+for row in rows:
+    print("Title : {} | View : {}".format(row[0], row[1]))
 
+sql = " select name from authors \
+        where id in \
+        (select a.author from log as l \
+        join articles as a on l.path=CONCAT('/article/',a.slug) \
+        group by l.path, a.author order by count(l.path) desc limit 3) \
+        order by name"
+c.execute(sql)
+rows = c.fetchall()
 print("")
 print("The Most 3 Popular Authors")
-for log in logs:
-    slug = log[0]
-    sql = "select author from articles where slug=%s"
-    c.execute(sql, [slug])
-    article = c.fetchall()
-    author_id = article[0][0]
-    sql = "select name from authors where id=%s"
-    c.execute(sql, [author_id])
-    author = c.fetchall()
-    author_name = author[0][0]
-    print("Author : {} | view : {}".format(author_name, log[1]))
+for row in rows:
+    print("Author : {}".format(row[0]))
 
 # Created 2 views
 sql = "select sc.day, round((100.0 * (ec.cnt::numeric/sc.cnt::numeric)), 1) \
@@ -48,7 +42,6 @@ print("")
 print("More than 1% of errors on")
 for log in logs:
     print("Date : {} | Error Rate : {}%".format(log[0], log[1]))
-
 print("")
 
 db.close()
